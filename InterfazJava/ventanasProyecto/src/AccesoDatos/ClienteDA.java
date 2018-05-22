7192d5de03aceb9ddc819a0857c8524d98689a0f
locoxsoco <a20151110@pucp.pe>
2018-05-22 12:44:37 -0500

    empezando ClienteDA y ClienteBL

---
+++ InterfazJava/ventanasProyecto/src/AccesoDatos/ClienteDA.java
@@ -0,0 +1,14 @@
+/*
+ * Nombre: Luis Alberto Carranza Cobenas
+ * Codigo: 20151110
+ * and open the template in the editor.
+ */
+package AccesoDatos;
+
+/**
+ *
+ * @author Luis Alberto Carranza Cobenas <20151110>
+ */
+public class ClienteDA {
+    
+}
